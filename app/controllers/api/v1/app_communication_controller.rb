d5a570d95afe6194e39d95e5bd129dda01967cdb
Eduardo Sánchez Yépiz <gallo.sanchez23@gmail.com>
2017-08-27 10:34:24 -0500

    Creates AppCommunication controller and first function of the api

---
+++ app/controllers/api/v1/app_communication_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::AppCommunicationController < ApplicationController
+	require 'json'
+
+	def show
+		json_data = JSON.parse(params[:json])
+		logger.info('json information: ' + json_data)
+		respond_to do |format|
+			format.json{
+      	bars_code = json_data['product']['bars_code']
+      	@product = Product.find_by(bars_code: bars_code)
+				json_response(@product)
+			}
+    end
+	end
+end
