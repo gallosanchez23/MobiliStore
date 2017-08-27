be49494971f0c62c03c4d80e58631ade58c7a8a3
Eduardo Sánchez Yépiz <gallo.sanchez23@gmail.com>
2017-08-27 11:25:04 -0500

    Modifies api functions

--- app/controllers/api/v1/app_communication_controller.rb
+++ app/controllers/api/v1/app_communication_controller.rb
@@ -8,7 +8,8 @@ class Api::V1::AppCommunicationController < ApplicationController
 			format.json{
       	bars_code = json_data['product']['bars_code']
       	@product = Product.find_by(bars_code: bars_code)
-				json_response(@product)
+				#json_response(@product)
+				logger.info(@product.inspect)
 			}
     end
 	end
